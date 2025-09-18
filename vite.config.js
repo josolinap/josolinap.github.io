d57408a4e3c81b15ccba8394d3118416d5584e0f
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-18 15:08:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/josolinap.github.io',
+  base: '/josolinap.github.io/',
   plugins: [react()],
 })
